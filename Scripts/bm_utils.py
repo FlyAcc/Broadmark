fde54ba9a4c046f70a10a235a3d799ad91269add
FlyAcc <1164825073@qq.com>
2022-01-01 17:11:01 +0800

    Update bm_utils.py
    Fix Error: "import collections collections.iterable not found"

--- Scripts/bm_utils.py
+++ Scripts/bm_utils.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 from __future__ import print_function, unicode_literals, with_statement, division, absolute_import
-import collections
 
 def get_iterable(x):
-    if isinstance(x, collections.Iterable):
-        return x
+    try:
+        iter(x)
+    except TypeError:
+        return (x, )
     else:
-        return (x,)
+        return x
